595c739aea5e12a257e6009089f37d4c08ddfc58
RodrigoFelixdev <srodrigofelix106@gmail.com>
2025-05-04 04:01:09 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿//https://discord.com/oauth2/authorize?client_id=1368428590177325127&permissions=8&integration_type=0&scope=bot
+
+using System;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using Discord;
+using Discord.WebSocket;
+using DotNetEnv;
+Env.Load();
+var discordToken = Environment.GetEnvironmentVariable("DiscordToken");
+
+async Task Runbotasync(){
+    
+    var client =  new DiscordSocketClient(new DiscordSocketConfig{
+        LogLevel = LogSeverity.Debug
+    });
+
+     await client.LoginAsync(TokenType.Bot, discordToken);
+
+     Console.WriteLine(client.LoginState);
+     
+     await client.StartAsync();
+
+     client.Ready += async () => 
+     {
+        var guild = client.GetGuild(1058834781598666772);
+        var channel = guild.GetTextChannel(1058834782177464371);
+        if (channel != null)
+        {
+            await channel.SendMessageAsync("olá");
+        }
+
+          await channel.SendMessageAsync("Olá mundo");
+
+        var embed = new EmbedBuilder();
+        embed.WithImageUrl("https://i.pinimg.com/736x/4a/69/f9/4a69f919d54deb48439a26e0e8260cc2.jpg");
+        await channel.SendMessageAsync(embed: embed.Build());
+
+        
+     };
+     client.Log +=  (log) => {
+        Console.WriteLine($"{DateTime.Now} -> {log.Message}");
+        return Task.CompletedTask;
+     };
+     var exitSignal =  new TaskCompletionSource<bool>();
+     await exitSignal.Task;
+
+
+
+}
+await Runbotasync();
+
+Console.ReadKey();
